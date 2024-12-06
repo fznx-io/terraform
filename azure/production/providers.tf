93ed3a97d5f549aff99415df6791a69d800b14c5
Fauzan <mufahaza@gmail.com>
2024-12-06 19:15:00 +0800

    Configure Azure provider and remote state backend

---
+++ azure/production/providers.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = "1.5.7"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+
+    virtual_machine {
+      delete_os_disk_on_deletion     = true
+      skip_shutdown_and_force_delete = false
+    }
+  }
+
+  subscription_id = var.subscription_id
+}
